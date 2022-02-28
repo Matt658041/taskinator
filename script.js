198d20cce953bb29ab3b8eb30aed0a6383fc948b
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-02-28 13:17:00 -0600

    updatedjs

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let buttonE1 = document.querySelector(`#save-task`);
+let formE1 = document.querySelector(`#task-form`);
 let tasksToDoE1 = document.querySelector(`#tasks-to-do`);
 
-let createTaskHandler = function() {
+let createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     let listItemE1 = document.createElement(`li`);
     listItemE1.className = `task-item`;
     listItemE1.textContent = `This is a new task`;
     tasksToDoE1.appendChild(listItemE1);
 };
-buttonE1.addEventListener(`click`,createTaskHandler); 
+formE1.addEventListener(`submit`,createTaskHandler); 
